f06b9753545bee0f0840bf1dab9721253ad5f234
HuynhTrang0912 <114849501+HuynhTrang0912@users.noreply.github.com>
2024-01-24 23:04:48 +0700

    Add files via upload

---
+++ Car_website/form-handler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'toyotachinhhang.com';
+
+$email_subject = 'New form submission';
+
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
+
+$to = 'huynhthai1123@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply_To: $visitor_email \r\n";
+
+mail($to,$email_subject,$visitor_body,$headers);
+
+header("location: contact.html");
+?>
